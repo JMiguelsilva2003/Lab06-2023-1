ef40c7e9e3306af0eab49deba98623b56edbdcdb
José Miguel da Silva <132301231+JMiguelsilva2003@users.noreply.github.com>
2023-12-14 20:44:08 +0000

    pratica lab 6

--- src/main/ordenadores/OrdenarPorContagemSimples.java
+++ src/main/ordenadores/OrdenarPorContagemSimples.java
@@ -1,11 +1,30 @@
 package ordenadores;
 
-public class OrdenarPorContagemSimples implements Ordenador{
+public class OrdenarPorContagemSimples implements Ordenador {
 
     @Override
     public int[] ordene(int[] array) {
+        int max = 0;
+        for (int i : array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        int[] countArray = new int[max + 1];
+
+        for (int i : array) {
+            countArray[i]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < countArray.length; i++) {
+            while (countArray[i] > 0) {
+                array[index++] = i;
+                countArray[i]--;
+            }
+        }
 
         return array;
     }
-    
 }
--- src/main/ordenadores/OrdenarQuick.java
+++ src/main/ordenadores/OrdenarQuick.java
@@ -8,7 +8,22 @@ public class OrdenarQuick implements Ordenador{
     }
 
     public int quickParticio(int[] array, int inicio, int fim){
-        return 0;
+            int pivot = array[inicio];
+            int i = inicio;
+        
+            for (int j = inicio + 1; j <= fim; j++) {
+                if (array[j] <= pivot) {
+                    i++;
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        
+            int temp = array[i];
+            array[i] = array[inicio];
+            array[inicio] = temp;
+        
+            return i;
+        }
     }
-    
-}
